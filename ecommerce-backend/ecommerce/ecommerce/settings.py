92017e1a50668cf8ef51e455b0d86f00e9b29fe6
krukd <krukd87@gmail.com>
2024-08-25 20:20:45 +0800

    Super user created

--- ecommerce-backend/ecommerce/ecommerce/settings.py
+++ ecommerce-backend/ecommerce/ecommerce/settings.py
@@ -120,7 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
 STATIC_URL = 'static/'
+
+MEDIA_URL='/images/'
+
+STATICFILES_DIRS =[
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT= 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
