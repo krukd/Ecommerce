f26dad6601c6484d5cfccd1519257d9721ddd908
krukd <krukd87@gmail.com>
2024-08-25 19:50:51 +0800

    Example of fetching products from server

--- ecommerce-frontend/src/components/screens/HomePage.jsx
+++ ecommerce-frontend/src/components/screens/HomePage.jsx
@@ -2,25 +2,39 @@ import React, { useEffect } from "react";
 import Header from "../Header";
 import { Container } from "react-bootstrap";
 import Footer from "../Footer";
-import {useState} from 'react'
-import axios from 'axios'
-
+import { useState } from "react";
+import axios from "axios";
+import { Row, Col } from "react-bootstrap";
 
 function HomePage() {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState()
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const {data} = await axios.get("/api/products/");
+        console.log(data);
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
-    useEffect(() => {
-        async function fetchProducts(){
-            const data = await axios.get('/api/products/')
-            setProducts(data)
-        }
-
-        fetchProducts()
-    })
+    fetchProducts();
+  }, []);
   return (
     <Container>
+      <br />
       <h1>Products</h1>
+      <Row>
+        {products.map((product) => {
+          return <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+            <h1>{product.product_name}</h1>
+            <h6>{product.category}</h6>
+            <p>{product.price}</p>
+          </Col>;
+        })}
+      </Row>
     </Container>
   );
 }
