c421d2e717847fcfaad7b987ea4e1f2e7cffec46
krukd <krukd87@gmail.com>
2024-08-25 18:24:48 +0800

    Simple fetching data is implemented

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -1,28 +1,30 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import {Container} from "react-bootstrap"
-import {HashRouter as Router, Routes, Route} from 'react-router-dom'
+// import { Container } from "react-bootstrap";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/screens/HomePage";
 import SignupPage from "./components/screens/SignupPage";
 import LoginPage from "./components/screens/LoginPage";
-
-
+import CartPage from "./components/screens/CartPage";
 
 function App() {
   return (
     <>
-    <Router>
-    <Header/>
-      <Routes>
-        <Route exact path="/" element={<HomePage/>}></Route>
-      </Routes>
-      <Routes>
-        <Route exact path="/login" element={<LoginPage/>}></Route>
-      </Routes>
-      <Routes>
-        <Route exact path="/signup" element={<SignupPage/>}></Route>
-      </Routes>
-    </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<HomePage />}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/login" element={<LoginPage />}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/signup" element={<SignupPage />}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/cart" element={<CartPage />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
--- ecommerce-frontend/src/components/screens/HomePage.jsx
+++ ecommerce-frontend/src/components/screens/HomePage.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import Header from '../Header'
-import { Container } from 'react-bootstrap'
-import Footer from '../Footer'
+import React, { useEffect } from "react";
+import Header from "../Header";
+import { Container } from "react-bootstrap";
+import Footer from "../Footer";
+import {useState} from 'react'
+import axios from 'axios'
 
 
 function HomePage() {
+
+    const [products, setProducts] = useState()
+
+    useEffect(() => {
+        async function fetchProducts(){
+            const data = await axios.get('/api/products/')
+            setProducts(data)
+        }
+
+        fetchProducts()
+    })
   return (
-    <div>
-      
-    </div>
-  )
+    <Container>
+      <h1>Products</h1>
+    </Container>
+  );
 }
 
-export default HomePage
+export default HomePage;
