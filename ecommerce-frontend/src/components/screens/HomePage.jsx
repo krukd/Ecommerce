5faf09ed206b9578ec0d0e445eefc0c402efe0ec
krukd <krukd87@gmail.com>
2024-08-27 00:08:28 +0800

    Loader and Message components are added

--- ecommerce-frontend/src/components/screens/HomePage.jsx
+++ ecommerce-frontend/src/components/screens/HomePage.jsx
@@ -1,31 +1,54 @@
+
 import React, { useEffect } from "react";
 import { Container } from "react-bootstrap";
+import axios from "axios";
 import { Row, Col } from "react-bootstrap";
 import Product from "../Product";
 import { listProducts } from "../../actions/productsActions";
-import { useDispatch, useSelector } from "react-redux";
+import {useDispatch,useSelector} from 'react-redux'
+import Loader from "../Loader";
+import Message from "../Message";
+
+
 
 function HomePage() {
-  const dispatch = useDispatch();
-  const productsList = useSelector((state) => state.productsList);
-  const { error, loading, products } = productsList;
-
+  const dispatch=useDispatch()
+  const productsList = useSelector((state)=>state.productsList);
+  const {error,loading,products}=productsList
+  
   useEffect(() => {
-    dispatch(listProducts());
+  dispatch(listProducts())
   }, [dispatch]);
+
+
+ 
   return (
     <Container>
       <br />
-      <h1>Products</h1>
+      <h1>Products </h1>
+      
+
+      {
+        loading?(
+          <Loader/>
+        ):error ? (
+     <Message variant='danger'>{error}</Message>
+        ):(
+          
       <Row>
-        {products.map((product) => {
-          return (
-            <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          );
-        })}
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+
+            <Product product={product} />
+            
+
+          </Col>
+        ))}
       </Row>
+        )
+      }
+
+
     </Container>
   );
 }
--- ecommerce-frontend/src/components/screens/ProductPage.jsx
+++ ecommerce-frontend/src/components/screens/ProductPage.jsx
@@ -12,25 +12,24 @@ import {
 import Rating from "../Rating";
 import { useDispatch, useSelector } from "react-redux";
 import { listProductDetails } from "../../actions/productsActions";
-
-
+import Message from "../Message";
+import Loader from "../Loader";
 
 function ProductPage({ params }) {
   const { id } = useParams();
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
   const { error, loading, product } = productDetails;
-  
 
   useEffect(() => {
-    dispatch(listProductDetails(id))
+    dispatch(listProductDetails(id));
   }, [dispatch, params]);
 
-  const MAX_LENGTH = 200; 
+  const MAX_LENGTH = 200;
 
   const truncateText = (text, maxLength) => {
     if (text && text.length > maxLength) {
-      return text.slice(0, maxLength) + '...';
+      return text.slice(0, maxLength) + "...";
     }
     return text;
   };
@@ -41,63 +40,70 @@ function ProductPage({ params }) {
         <Link to="/" className="btn btn-dark my-3">
           Go Back
         </Link>
-        <Row>
-          <Col md={6}>
-            <Image src={product.image} alt={product.name} fluid />
-          </Col>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Row>
+            <Col md={6}>
+              <Image src={product.image} alt={product.name} fluid />
+            </Col>
 
-          <Col md={3}>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                <h3>{product.productname}</h3>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Rating
-                  value={product.rating}
-                  text={`${product.numReviews} reviews`}
-                  color={"#f8e825"}
-                />
-              </ListGroup.Item>
-              <ListGroup.Item>Brand: {product.productbrand} </ListGroup.Item>
-
-              <ListGroup.Item>
-                Description: {truncateText(product.productinfo, MAX_LENGTH)}
-              </ListGroup.Item>
-            </ListGroup>
-          </Col>
-
-          <Col md={3}>
-            <Card>
+            <Col md={3}>
               <ListGroup variant="flush">
                 <ListGroup.Item>
-                  <Row>
-                    <Col>Price:</Col>
-                    <Col>
-                      <strong>{product.price} Rs</strong>
-                    </Col>
-                  </Row>
+                  <h3>{product.productname}</h3>
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  <Row>
-                    <Col>Status:</Col>
-                    <Col>
-                      {product.stockcount > 0 ? "In Stock" : "Out of Stock"}
-                    </Col>
-                  </Row>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                    color={"#f8e825"}
+                  />
                 </ListGroup.Item>
+                <ListGroup.Item>Brand: {product.productbrand} </ListGroup.Item>
+
                 <ListGroup.Item>
-                  <Button
-                    className="btn-block btn-success"
-                    disabled={product.stockcount == 0}
-                    type="button"
-                  >
-                    Add to Cart
-                  </Button>
+                  // Description:{" "}
+                  {truncateText(product.productinfo, MAX_LENGTH)}
                 </ListGroup.Item>
               </ListGroup>
-            </Card>
-          </Col>
-        </Row>
+            </Col>
+
+            <Col md={3}>
+              <Card>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price:</Col>
+                      <Col>
+                        <strong>{product.price} Rs</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        {product.stockcount > 0 ? "In Stock" : "Out of Stock"}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Button
+                      className="btn-block btn-success"
+                      disabled={product.stockcount == 0}
+                      type="button"
+                    >
+                      Add to Cart
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        )}
       </div>
     </Container>
   );
